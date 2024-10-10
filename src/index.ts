91312ece011e22ef6e7f25d367fc93cbea1ac13a
enderNakamoto <ender.nakamoto@gmail.com>
2024-10-09 21:02:40 -0700

    getting weather and calling set weather

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { Account, Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+import { Account, Aptos, AptosConfig, Ed25519PrivateKey, Network } from "@aptos-labs/ts-sdk";
 
-const WEBHOOK_URL = 'https://webhook.site/c86183b2-ae9b-47e6-b48c-3f5c975825fc';
 const BASE_URL = 'https://api.openweathermap.org/';
 const LOCATION = 'Tokyo';
 const API_KEY = 'dbc6b0f32de6d95d3885a9ca7a774bd9';
+const WEATHERMAN_PK = '0xa953ef08e9a2bc62a61fb7f96eebb05091b50d966545ba993562dd0ce13d51a8';
+const WARLORD_ADDRESS = '0xba50f4b5a6b6d5dce1d181f957a13bb0e344bc71f83f496b461ba4340f749a6e';
+
+enum WeatherConditionEnum {
+  CLEAR = "0",
+  CLOUDS = "1",
+  SNOW = "2",
+  RAIN = "3",
+  DRIZZLE = "4",
+  THUNDERSTORM = "5",
+}
 
 declare const _STD_: any;
 
@@ -18,36 +28,91 @@ if (typeof _STD_ === "undefined") {
   };
 }
 
-const config = new AptosConfig({ network: Network.TESTNET });
-const aptos = new Aptos(config);
+async function getWeatherData() {
+  const response = await fetch(`${BASE_URL}data/2.5/weather?q=${LOCATION}&appid=${API_KEY}`);
+  const data = await response.json();
+  return data["weather"][0]["main"];
+}
 
-// Generate two account credentials
-// Each account has a private key, a public key, and an address
-const alice = Account.generate();
-const bob = Account.generate();
+function weatherConditionToNumber(condition: string): string {
+  switch (condition.toUpperCase()) {
+    case 'CLEAR':
+      return WeatherConditionEnum.CLEAR;
+    case 'CLOUDS':
+      return WeatherConditionEnum.CLOUDS;
+    case 'SNOW':
+      return WeatherConditionEnum.SNOW;
+    case 'RAIN':
+      return WeatherConditionEnum.RAIN;
+    case 'DRIZZLE':
+      return WeatherConditionEnum.DRIZZLE;
+    case 'THUNDERSTORM':
+      return WeatherConditionEnum.THUNDERSTORM;
+    default:
+      console.warn(`Unknown weather condition: ${condition}. Defaulting to CLEAR.`);
+      return WeatherConditionEnum.CLEAR;
+  }
+}
 
-// send it to your backend
-fetch(`${BASE_URL}data/2.5/weather?q=${LOCATION}&appid=${API_KEY}`)
-  .then((response) => response.json())
-  .then((data) => {
-    const condition = data["weather"][0]["main"];
-    return fetch(WEBHOOK_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        condition,
-        timestamp: Date.now(),
-        acurast: {
-          version: _STD_.app_info.version,
-          deploymentId: _STD_.job.getId(),
-          deviceAddress: _STD_.device.getAddress(),
-        },
-      }),
-    })
-      .then((postResponse) => console.log("Success:", postResponse.status))
-      .catch((error) => console.error("Error posting data:", error));
-  })
-  .catch((error) => console.error("Error getting data:", error));
+async function callWeatherChange(weatherCondition: string) {
+  // Setup the client
+  const config = new AptosConfig({ network: Network.TESTNET });
+  const aptos = new Aptos(config);
+
+
+  // get the weatherman's account
+  const privateKey = new Ed25519PrivateKey(WEATHERMAN_PK);
+  const weatherman  = Account.fromPrivateKey({ privateKey });
+  console.log(`weatherman's address is: ${weatherman.accountAddress}`);
+  
+
+  // build the transaction
+  const txn = await aptos.transaction.build.simple({
+    sender: weatherman.accountAddress,
+    data: {
+      // All transactions on Aptos are implemented via smart contracts.
+      function: `${WARLORD_ADDRESS}::warlords::set_weather`,
+      functionArguments: [weatherCondition],
+    },
+  });
+ 
+  // Both signs and submits
+  console.log("signing and submitting transaction ...");
+  const committedTxn = await aptos.signAndSubmitTransaction({
+    signer: weatherman,
+    transaction: txn,
+  });
+
+  // Waits for Aptos to verify and execute the transaction
+  console.log("waiting for transaction to be executed ...");
+  const executedTransaction = await aptos.waitForTransaction({
+    transactionHash: committedTxn.hash,
+  });
+
+  console.log("Transaction hash:", executedTransaction.hash);
+}
+
+async function fetchAndPostWeatherData(): Promise<void> {
+  try {
+    const condition = await getWeatherData();
+    const conditionNumber = weatherConditionToNumber(condition);
+    try {
+      const status = await callWeatherChange(conditionNumber);
+      console.log("Success:", status);
+    } catch (error) {
+      console.error("Error posting data:", error);
+    }
+  } catch (error) {
+    console.error("Error getting data:", error);
+  }
+}
+
+fetchAndPostWeatherData();
+
+
+
+
+
 
 
 
